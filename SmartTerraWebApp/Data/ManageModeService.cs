7c7d6c8f48ee6687b766356508829c72a3728863
acwikla <acwikla.13@gmail.com>
2020-12-20 01:01:24 +0100

    Small view changes.

---
+++ SmartTerraWebApp/Data/ManageModeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartTerraWebApp.Data
+{
+    public class ManageModeService
+    {
+        // oninitial override: GET: api/devices/{deviceId}/modes=> in null =>alert:"Please add mode."
+        // PATCH: api/IsOn/{modeId}
+        // POST: api/devices/{deviceId}/modes
+        // PUT: api/Modes/{modeId}
+    }
+}
